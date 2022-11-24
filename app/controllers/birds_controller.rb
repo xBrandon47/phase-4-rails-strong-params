acf3f3f71cff8414a8b657ac573fc7a07cbc2223
Brandon Moreno <brandonm99@me.com>
2022-11-24 11:34:40 -0700

    commit tree f806b48f40ceec52a4b28036e958024084b7f582

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,8 +8,17 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
   # GET /birds/:id
